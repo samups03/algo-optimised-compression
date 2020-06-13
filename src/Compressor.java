ea247208cff50bd1b77455478828775d39670602
Samups <baseballsam0301@gmail.com>
2020-06-13 00:56:08 -0400

    Working on compression v.1.2

--- src/Compressor.java
+++ src/Compressor.java
@@ -1,13 +1,14 @@
 import java.nio.ByteBuffer;
 import java.nio.ShortBuffer;
 import java.lang.*;
+import java.util.HashMap;
 import java.util.Objects;
 
 public class Compressor {
     private final int DICTIONNARY_SIZE = 256;
     private byte[] array;
-    private byte[] dictionnary = new byte[2048];
-    private int compteur=DICTIONNARY_SIZE;
+    private HashMap<String, Integer> dictionnary = new HashMap<String, Integer>(512);
+    private int compteur=DICTIONNARY_SIZE+1;
     private ByteBuffer buffer;
     private String byteString;
 
@@ -19,62 +20,53 @@ public class Compressor {
     public int compress(){
         int returnValue = 0;
         byte tempUse;
-        byte cursor = array[0];
+        Byte cursor = array[0];
+        String cursorStr = Character.toString((char) array[0]);
+        String tempString ="";
 
         initialiseDictionnary();
 
         for(int i = 1; i< array.length; i++){
-            byte tempByte = this.array[i];
-            if(i<array.length - 1) {
-                cursor = this.array[i+1];
-            }
+            Byte tempByte = this.array[i];
+            String tempByteStr = Character.toString((char)this.array[i]);
+            int tempByteStringInt = 0;
+            tempString = cursorStr + tempByteStr;
             tempUse = (byte) (cursor + tempByte);
 
-            if(elementOfArray(tempByte, dictionnary)){
-                cursor = (byte) (cursor + tempByte);
-                while(elementOfArray(cursor, dictionnary)){
-                    tempByte = array[i+2];
-                    cursor = (byte)(cursor<<8 | tempByte);
-                }
-                System.out.print(cursor);
-                dictionnary[compteur] = cursor;
+            if(dictionnary.containsKey(tempString)){
+                cursorStr = tempString;
             }
+//            if(elementOfArray(tempByte, dictionnary)){
+//                cursor = (byte) (cursor + tempByte);
+//                while(elementOfArray(cursor, dictionnary)){
+//                    tempByte = array[i+2];
+//                    cursor = (byte)(cursor<<8 | tempByte);
+//                }
+//                System.out.print(cursor);
+//                dictionnary[compteur] = cursor;
+//            }
             else{
-                dictionnary[compteur] = tempByte;
-                cursor = tempByte;
+                tempByteStringInt = Integer.parseInt(String.valueOf((Byte.toUnsignedInt(cursor) + Byte.toUnsignedInt(tempByte))));
+                dictionnary.put(tempString, compteur);
+                cursorStr = tempByteStr;
+                compteur +=1;
             }
-            compteur +=1;
         }
 
-        buffer = ByteBuffer.wrap(dictionnary);
-        returnValue = buffer.get();
+        //buffer = ByteBuffer.wrap(dictionnary);
+        //returnValue = buffer.get();
 
-        return returnValue;
+        //return returnValue;
 
-
+        return 1;
     }
 
     private void initialiseDictionnary(){
 
         for(int i = 0; i<DICTIONNARY_SIZE; i++){
             char tempCharacter = (char)i;
-            dictionnary[i] = (byte)tempCharacter;
+            dictionnary.put(Character.toString(tempCharacter), i);
         }
     }
-
-    private boolean elementOfArray(byte texte, byte[] dictionnary){
-        boolean value = false;
-        boolean output;
-
-        for(byte item: dictionnary){
-            output = Objects.equals(texte, item);
-            if(output);
-               value = true;
-        }
-        return value;
-    }
-
-
-
 
 }
